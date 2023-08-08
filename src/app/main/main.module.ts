29df466406a4d2491a256a65ea5683c073167f13
pawel <pawelkozka10@gmail.com>
2023-08-08 17:56:25 +0200

    add angular material

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { MainRoutingModule } from './main-routing.module';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, MainRoutingModule],
+    imports: [CommonModule, MainRoutingModule, MatButtonModule],
 })
 export class MainModule {}
